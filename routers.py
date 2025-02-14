ac7e43f14dee51caf07e32d4792c6bc1865362fc
yraeel <israelmachado19@outlook.com>
2025-02-14 09:48:42 -0300

    atualização dos endpoints

--- routers.py
+++ routers.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask,jsonify, request, Response
 import requests
+import csv
+import io
+import pandas as pd
+
 app = Flask(__name__)
 
 TOKEN = "ProcessoSeletivoStract2025"
@@ -20,38 +24,21 @@ def get_platforms():
     url = "https://sidebar.stract.to/api/platforms"
 
     headers = {
-        "Authorization": f"Bearer {TOKEN}",  # Adiciona o token no cabeçalho
-        "Content-Type": "application/json"   # Especifica que queremos JSON
+        "Authorization": f"Bearer {TOKEN}",  
+        "Content-Type": "application/json"  
     }
 
     try:
-        # Faz a requisição GET com o token
+        
         response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Verifica se a resposta foi bem-sucedida (200 OK)
+        response.raise_for_status()  
 
-        data = response.json()  # Converte a resposta para JSON
-        return jsonify(data)  # Retorna os dados para o cliente Flask
+        data = response.json()  
+        return jsonify(data)  
     except requests.exceptions.RequestException as e:
         return jsonify({"error": f"Erro ao conectar à API: {str(e)}"}), 500
 
 
-
-@app.route('/accounts', methods=['GET'])
-def get_accounts():
-    platform = request.args.get('platform')
-    url = f"https://sidebar.stract.to/api/accounts?platform={platform}"
-    headers = {
-    "Authorization": f"Bearer {TOKEN}",
-    "Content-Type": "application/json"
-    }
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        data = response.json()
-        return jsonify(data)
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": f"Erro ao conectar à API: {str(e)}"}), 500
 
 
 @app.route('/fields', methods=['GET'])
@@ -94,8 +81,68 @@ def get_insights():
 
 
 
+@app.route('/accounts', methods=['GET'])
+def get_accounts():
+    platform = request.args.get('platform')
+    url = f"https://sidebar.stract.to/api/accounts?platform={platform}"
+    headers = {
+    "Authorization": f"Bearer {TOKEN}",
+    "Content-Type": "application/json"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return jsonify(data)
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": f"Erro ao conectar à API: {str(e)}"}), 500
+
+
+@app.route('/<platform>', methods=['GET'])
+def get_ads(platform):
+
+    url_platform = f'https://sidebar.stract.to/api/fields?platform={platform}'
+    headers = {
+    "Authorization": f"Bearer {TOKEN}",
+    "Content-Type": "application/json"
+    }
+
+    
+    try:
+        response = requests.get(url_platform, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        
+        
+        if "fields" in data:
+            field_values = [field["value"] for field in data["fields"]]
+            print("Valores dos campos:", field_values)  
+
+            return jsonify(field_values)
+
+            
+        else:
+            return jsonify({"error": "Resposta inesperada da API"}), 500
 
     
 
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": f"Erro ao conectar à API: {str(e)}"}), 500
+
+
+   
+
 if __name__ == '__main__':
     app.run()
+
+
+"""
+iterar a lista de plataformas
+e para cada plataforma iterar as contas de cada
+e para cada conta iterar os insights
+"""
+
+"""
+
+"""
